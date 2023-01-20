efe5fca1e3789d945478e8b69b3cf9739b2a621b
Bradley <bradleypetit5@gmail.com>
2023-01-20 14:17:16 -0500

    gg

--- BillZzapp/ViewController.swift
+++ BillZzapp/ViewController.swift
@@ -2,16 +2,18 @@
 //  ViewController.swift
 //  BillZzapp
 //
-//  Created by Petit Bradley on 1/6/23.
+//  Created by ANT on 1/6/23.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heading: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
