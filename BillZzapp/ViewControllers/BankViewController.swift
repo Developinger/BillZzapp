1436122abd4590dbdb16cce704862ec6569c8978
Daniel <danielbonneau04@gmail.com>
2023-05-17 14:08:14 -0400

    tt

---
+++ BillZzapp/ViewControllers/BankViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BankViewController.swift
+//  BillZzapp
+//
+//  Created by 7g on 5/17/23.
+//
+
+import UIKit
+
+
+class BankViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
