9bc6a30b38754fc0ffc242380df7099a21308dfc
Daniel <danielbonneau04@gmail.com>
2023-05-23 14:18:45 -0400

    ss

--- BillZzapp/ViewControllers/PersonalInformationViewController.swift
+++ BillZzapp/ViewControllers/PersonalInformationViewController.swift
@@ -17,141 +17,84 @@ class PersonalInformationViewController: UIViewController, MKLocalSearchComplete
     @IBOutlet weak var zipTextField: UITextField!
     @IBOutlet weak var stateTextField: UITextField!
     @IBOutlet weak var countryPicker: UIPickerView!
-
-    var completer: MKLocalSearchCompleter = MKLocalSearchCompleter()
-    var searchResults: [MKLocalSearchCompletion] = []
-    var searchTimer: Timer?
-
-    var countries: [String] = {
-        let countryCodes = NSLocale.isoCountryCodes
-        var names: [String] = []
-        for countryCode in countryCodes {
-            let id = NSLocale.localeIdentifier(fromComponents: [NSLocale.Key.countryCode.rawValue: countryCode])
-            let name = Locale(identifier: "en_US").localizedString(forRegionCode: id) ?? ""
-            names.append(name)
+    let completer = MKLocalSearchCompleter()
+        var completerResults = [MKLocalSearchCompletion]()
+        
+        let countries = Locale.isoRegionCodes.compactMap { (code) -> String? in
+            Locale.current.localizedString(forRegionCode: code)
+        }.sorted()
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            completer.delegate = self
+            completer.resultTypes = .address
+            addressTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+            countryPicker.dataSource = self
+            countryPicker.delegate = self
+            tableView.delegate = self
+            tableView.dataSource = self
+            tableView.isHidden = true
         }
-        names.sort() // Sort the countries alphabetically
-        return names
-    }()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        completer.delegate = self
-        completer.resultTypes = .address
-
-        countryPicker.delegate = self
-        countryPicker.dataSource = self
-
-        addressTextField.delegate = self
-
-        hideTableView()
-    }
-
-    @IBAction func textFieldChanged(_ sender: UITextField) {
-        searchTimer?.invalidate()
-
-            guard let query = sender.text else {
-                hideTableView()
-                return
-            }
-
-            if query.isEmpty {
-                hideTableView()
-            } else {
-                showTableView()
-            }
-
-            searchTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: false) { _ in
-                self.completer.queryFragment = query
-            }
+        
+        @objc func textFieldDidChange(_ textField: UITextField) {
+            completer.queryFragment = textField.text ?? ""
         }
-
-    @objc func search() {
-        completer.queryFragment = addressTextField.text ?? ""
-    }
-
-    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
-        print("completerDidUpdateResults called")
-        searchResults = completer.results
-            if searchResults.isEmpty {
-                hideTableView()
-            } else {
-                showTableView()
-            }
+        
+        // MARK: - MKLocalSearchCompleterDelegate methods
+        
+        func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+            completerResults = completer.results
             tableView.reloadData()
+            tableView.isHidden = false
         }
-
-    func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
-        // handle error
-    }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return searchResults.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
-        let searchResult = searchResults[indexPath.row]
-        cell.textLabel?.text = searchResult.title
-        cell.detailTextLabel?.text = searchResult.subtitle
-
-        return cell
-    }
-
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let searchResult = searchResults[indexPath.row]
-        let searchRequest = MKLocalSearch.Request(completion: searchResult)
-        let search = MKLocalSearch(request: searchRequest)
-        search.start { [weak self] (response, error) in
-            guard let response = response else { return }
-
-            let mapItem = response.mapItems[0]
-            let placemark = mapItem.placemark
-
-            var addressString = ""
-            if let subThoroughfare = placemark.subThoroughfare {
-                addressString += subThoroughfare + " "
-            }
-            if let thoroughfare = placemark.thoroughfare {
-                addressString += thoroughfare
-            }
-            self?.addressTextField.text = addressString
-            self?.zipTextField.text = placemark.postalCode
-            self?.stateTextField.text = placemark.administrativeArea // state
-            self?.cityTextField.text = placemark.locality // city
-
-            if let country = placemark.country {
-                if let index = self?.findCountryIndex(country) {
-                    self?.countryPicker.selectRow(index, inComponent: 0, animated: true)
+        
+        // MARK: - TableView methods
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return completerResults.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = completerResults[indexPath.row].title
+            return cell
+        }
+        
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            
+            
+            let result = completerResults[indexPath.row]
+            let searchRequest = MKLocalSearch.Request(completion: result)
+            let search = MKLocalSearch(request: searchRequest)
+            search.start { (response, error) in
+//                let placemark = response?.mapItems[0].placemark
+//                let fullAddress = "\(placemark?.subThoroughfare ?? ""), \(placemark?.thoroughfare ?? ""), \(placemark?.locality ?? ""), \(placemark?.administrativeArea ?? ""), \(placemark?.postalCode ?? ""), \(placemark?.country ?? "")"
+//                self.addressTextField.text = fullAddress
+                let coordinate = response?.mapItems[0].placemark.coordinate
+           
+                //self.addressTextField.text = response?.mapItems[0].placemark.name
+                self.zipTextField.text = response?.mapItems[0].placemark.postalCode
+                self.stateTextField.text = response?.mapItems[0].placemark.administrativeArea
+                self.cityTextField.text = response?.mapItems[0].placemark.locality
+                let country = response?.mapItems[0].placemark.country
+                if let index = self.countries.firstIndex(of: country ?? "") {
+                    self.countryPicker.selectRow(index, inComponent: 0, animated: true)
                 }
+                self.tableView.isHidden = true
             }
-
-            self?.hideTableView()
+        }
+        
+        // MARK: - UIPickerViewDelegate and DataSource methods
+        
+        func numberOfComponents(in pickerView: UIPickerView) -> Int {
+            return 1
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            return countries.count
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+            return countries[row]
         }
     }
-
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return countries.count
-    }
-
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return countries[row]
-    }
-
-    func findCountryIndex(_ country: String) -> Int? {
-        return countries.firstIndex(where: { $0 == country })
-    }
-
-    func showTableView() {
-        self.tableView.isHidden = false
-    }
-
-    func hideTableView() {
-        self.tableView.isHidden = true
-    }
-}
